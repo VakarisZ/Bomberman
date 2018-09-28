77d4fe0611b2846656927985be704c248a32f478
ashirviskas <minematas@gmail.com>
2018-09-28 13:43:02 +0300

    initial server backend, simple tick system

---
+++ src/ServerBackend/Connector.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServerBackend;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mati
+ */
+public class Connector {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(4000);
+        try {
+            Socket socket = serverSocket.accept();
+            while (true) {
+
+                DataOutputStream data_out = new DataOutputStream(socket.getOutputStream());
+                data_out.writeByte(0);
+                data_out.flush();
+                TimeUnit.SECONDS.sleep(1);
+                
+
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Connector.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            serverSocket.close();
+        }
+    }
+}
