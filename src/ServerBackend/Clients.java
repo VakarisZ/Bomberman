d3272859dc9901280f04916cdeb7f310a84833c7
ashirviskas <minematas@gmail.com>
2018-10-26 02:33:12 +0300

    Initial observer classes

---
+++ src/ServerBackend/Clients.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServerBackend;
+
+import java.util.Observable;
+
+/**
+ *
+ * @author mati
+ */
+public class Clients extends Observable{
+    
+}
