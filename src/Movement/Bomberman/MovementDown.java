f906f0d5c92647aab45156b7de92c1ebd9a8b578
VakarisZ <vakarisz@yahoo.com>
2018-10-28 22:15:00 +0200

    Map added to client board

---
+++ src/Movement/Bomberman/MovementDown.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Movement.Bomberman;
+
+import Movement.BombermanMovement;
+
+/**
+ *
+ * @author Vakaris
+ */
+public class MovementDown {
+    
+}
